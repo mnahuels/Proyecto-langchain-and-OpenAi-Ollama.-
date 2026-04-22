d8d56ead7f8b1269873314f256e1157a86b9dc53
nahuel salto <mnahuels01@gmail.com>
2026-04-22 10:48:16 -0300

    Uso de memoria con openai gpt-4o

--- modelo_openai.py
+++ modelo_openai.py
@@ -16,10 +16,39 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(model="GPT-4o")
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "Eres un psicologo que atiende a {Cosas}, y se especializaa en {Temas} ")
+    ("system", "Sos un soldado de guerra retirado, tienes que hablar en espanol y en terminos del ejercito sobre {experiencia} "),
+  MessagesPlaceholder(variable_name="historia"),
     ("human", "{consulta}"),
-    MessagesPlaceholder(variable_name="history"),
+    
 ])
 
 chain = prompt | llm
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+with_message_history = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key="consulta",
+    history_messages_key="historia",
+)
+with_message_history.invoke({"consulta": "Mi comida favorita es la pizza, Vos que comias en batalla ?", "experiencia": "segunda guerra mundial, batalla de stalingrado, vietnam"},
+config={"configurable": {"session_id": "abc123"}},),
+
+with_message_history.invoke({"consulta": "Que estrategia se solian usar en las trincheras", "experiencia": "segunda guerra mundial, batalla de stalingrado, vietnam"},
+config={"configurable": {"session_id": "abc123"}},),
+
+Respuesta=with_message_history.invoke({"consulta": "Recuerdas mi comida favorita ? Vos que comias en batalla ?", "experiencia": "segunda guerra mundial, batalla de stalingrado, vietnam"},
+config={"configurable": {"session_id": "abc123"}},),
+
+
 
